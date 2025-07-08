409d6b562c6770bb96baa951799481de111903cb
Oluwatomi-olasoji <91892024+Oluwatomi-olasoji@users.noreply.github.com>
2025-07-08 12:14:46 +0000

    mod test

--- zkp_auth/src/lib.rs
+++ zkp_auth/src/lib.rs
@@ -12,3 +12,49 @@ pub fn solve( k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint) -> BigUint {
     }
     return q - (c * x - k).modpow(&BigUint::from(1u32), q);
 }
+
+// the verfiy function verifies the solution by checking if 
+//r1 = A^s * y1^c and r2 = B^s * y2^c
+
+pub fn verify_solution( r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, A: &BigUint, B: &BigUint , c: &BigUint, s: &BigUint, p: &BigUint) -> bool {
+   let condition1 = *r1 == (A.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), &p);
+   let condition2 = *r2 == (B.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), &p);
+   condition1 && condition2
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn testing_zkp() {
+        let alpha = BigUint::from(4u32);  
+        let beta = BigUint::from(9u32);
+        let p = BigUint::from(23u32);
+        let q = BigUint::from(11u32);
+
+        let x = BigUint::from(6u32); //the secret
+        let k = BigUint::from(7u32); 
+
+        let c = BigUint::from(4u32);
+
+        let y1 = exponentiate(&alpha, &x, &p);
+        let y2 = exponentiate(&beta, &x, &p);
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1 = exponentiate(&alpha, &k, &p);
+        let r2 = exponentiate(&beta, &k, &p);
+        assert_eq!(r1, BigUint::from(8u32));
+        assert_eq!(r2, BigUint::from(4u32));
+
+        let s = solve(&k, &c, &x, &q);
+        assert_eq!(s, BigUint::from(5u32));
+
+        let result = verify_solution(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        assert!(result);
+
+
+    }
+    }
+
---
+++ zkp_auth/target/debug/deps/zkp_auth-b9d74b0d56c4c422.d
@@ -0,0 +1,7 @@
+/workspaces/rust-auth-project/zkp_auth/target/debug/deps/libzkp_auth-b9d74b0d56c4c422.rmeta: src/lib.rs
+
+/workspaces/rust-auth-project/zkp_auth/target/debug/deps/libzkp_auth-b9d74b0d56c4c422.rlib: src/lib.rs
+
+/workspaces/rust-auth-project/zkp_auth/target/debug/deps/zkp_auth-b9d74b0d56c4c422.d: src/lib.rs
+
+src/lib.rs:
