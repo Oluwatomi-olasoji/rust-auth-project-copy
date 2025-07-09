7caa966b0b3e44c4fbcf6e5e84b6af9c7e459567
Oluwatomi-olasoji <91892024+Oluwatomi-olasoji@users.noreply.github.com>
2025-07-09 01:50:18 +0000

    updated server execuatble

--- zkp_auth/src/server.rs
+++ zkp_auth/src/server.rs
@@ -1,19 +1,38 @@
 pub mod zkp_auth{
     include!("zkp_auth.rs");
 }
+use num_bigint::BigUint;
 use tonic::{transport::Server, Code,Request, Response, Status, codegen::http::request};
 use zkp_auth::auth_server::{Auth, AuthServer}; 
-use zkp_auth::{RegisterRequest, RegisterResponse};
-
-use crate::zkp_auth::{ChallengeRequest, ChallengeResponse, SolutionResponse, SolutionRequest};               // For the message struct
+use zkp_auth::{RegisterRequest, RegisterResponse,ChallengeRequest, ChallengeResponse, SolutionResponse, SolutionRequest};               // For the message struct
 
 #[derive(Debug, Default)]
 struct AuthImpl {}
 
+#[derive(Debug, Default)]
+pub struct UserInformation {
+    pub username: String,
+    pub y1: BigUint, //these two are used for registration
+    pub y2: BigUint,
+
+    pub r1: BigUint,//these two are used for authentication
+    pub r2: BigUint,
+
+    pub c: BigUint, //the below are used to verify the proof
+    pub s: BigUint,
+    pub session_id: String,
+}
+
 #[tonic::async_trait]
 impl Auth for AuthImpl {
     async fn register(&self, request:Request<RegisterRequest>) -> Result<Response<RegisterResponse>,Status> {
-        todo!()
+        let request = request.into_inner();
+
+        let y1 = BigUnit::from_bytes_be(&request.y1);
+        let y2 = BigUnit::from_bytes_be(&request.y2);
+
+        
+        Ok(Response::new(RegisterResponse { }))
 }
     async fn create_challenge(&self, request:Request<ChallengeRequest>) -> Result<Response<ChallengeResponse>,Status> {
         todo!()
